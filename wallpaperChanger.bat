7151c299aa4dffcc6a785fa8b505164e315f5e50
Rekhansh Panchal <rpanchal@uncc.edu>
2016-11-26 20:18:38 -0500

    Update wallpaperChanger.bat

--- wallpaperChanger.bat
+++ wallpaperChanger.bat
@@ -2,5 +2,5 @@ REM This batch file is used to run Python Script via Command Window.
 REM pythonw is the program linked to pythonw.exe via environment variables.
 REM The link after pythonw should include actual link to the Python program which needs to be run.
 cd C:\Users\Rekhansh\AppData\Local\Programs\Python\Python35-32
-python wallpaperChanger.py
+pythonw wallpaperChanger.py
 
